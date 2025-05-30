b803eda215b76bc8952eef6b374ae63b302cd346
vijayrajpanchall <vijayrajpanchal4.com>
2025-05-30 10:37:34 +0530

    created a basic react agent

--- 1_Introduction/react_agent_basic.py
+++ 1_Introduction/react_agent_basic.py
@@ -1,6 +1,36 @@
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain.agents import initialize_agent, tool
+from langchain_community.tools import TavilySearchResults
+import datetime
 
-llm = ChatOpenAI()
+load_dotenv()
 
-result = llm.invoke("What is the capital of France?")
-print(result)
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash-001")
+
+search_tool = TavilySearchResults(search_depth="basic")
+
+@tool
+def get_system_time(format: str = "%Y-%m-%d %H:%M:%S"):
+    """Get the current date and time in the specified format."""
+    current_time = datetime.datetime.now()
+    formatted_time = current_time.strftime(format)
+    return formatted_time
+
+
+tools = [search_tool, get_system_time]
+
+agent = initialize_agent(
+    tools=tools,
+    llm=llm,
+    agent_type="zero-shot-react-description",
+    verbose=True,
+)
+
+agent.invoke(
+    "When was Space'x last launch and how many days ago was that from this instant?"
+)
+
+# agent.invoke("Give me a funny tweet about the weather in Jaipur today. ")
+# result = llm.invoke("Date of today ")
+# print(result.content)
